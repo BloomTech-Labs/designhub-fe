833e1714c7b166d0e7ac5a76048dc635475e51d2
Mike VanSleen <mike.vansleen@gmail.com>
2019-09-11 15:35:21 -0600

    Added Provider, createStore to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 import './index.css';
 import App from './App';
 
+const store = createStore(() => {}, applyMiddleware(thunk, logger));
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
