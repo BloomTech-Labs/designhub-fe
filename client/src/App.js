08409be071ba3ace5e1b0dbe2f8bc1e91133a5b1
Mike VanSleen <mike.vansleen@gmail.com>
2019-09-12 12:00:53 -0600

    Auth0 initial setup done

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { useAuth0 } from './auth-wrapper.js';
+
 import './App.css';
 import UserProfile_LI from './components/UserProfile_LI';
+import NavBar from './components/NavBar.js';
+import FakeProfile from './components/FakeProfile.js';
 
 function App() {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
+      <NavBar />
+      <Route exact path="/" />
       <Route exact path="/profile" component={UserProfile_LI} />
+      <Route exact path="/fake-profile" component={FakeProfile} />
     </div>
   );
 }
