82f64d30442bb17097b5bf228d93c88a1a9f09e3
Mike VanSleen <mike.vansleen@gmail.com>
2019-09-12 17:44:14 -0600

    Changes App.js again

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,8 @@ import './App.scss';
 import UserProfile_LI from './components/UserProfile_LI';
 import { Route, Switch } from 'react-router-dom';
 import { useAuth0 } from './auth-wrapper.js';
-import './App.scss';
-import Navbar from './components/Navbar.js';
+import './App.css';
+import NavBar from './components/Navbar.js';
 import FakeProfile from './components/FakeProfile.js';
 import PrivateRoute from './components/PrivateRoute.js';
 
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar />
+      <NavBar />
       <Switch>
         <Route exact path="/" />
         <Route exact path="/profile" component={UserProfile_LI} />
