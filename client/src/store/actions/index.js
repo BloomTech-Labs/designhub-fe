306dfe5b820e8eed993e19ff4dabfe0a07dbf730
Mike VanSleen <mike.vansleen@gmail.com>
2019-09-12 14:54:02 -0600

    Added action to get all users and save to store

--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -15,3 +15,42 @@ export const LOGOUT_FAILURE = 'LOGIN_FAILURE';
 export const logout = () => dispatch => {
   dispatch({ type: LOGOUT_START });
 };
+
+ export const SIGNUP_START = 'SIGNUP_START',
+ export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS',
+ export const SIGNUP_FAILURE = 'SIGNUP_FAILURE',
+
+ export const signup = () => dispatch => {
+    dispatch({type: SIGNUP_START })
+ }
+
+ export const GET_USERS_START = 'GET_USERS_START',
+ export const GET_USERS_SUCCESS = 'GET_USERS_SUCCESS',
+ export const GET_USERS_FAILURE = 'GET_USERS_FAILURE',
+
+ export const getUsers = () => dispatch => {
+   dispatch({type: GET_USERS_START});
+   return axiosWithAuth()
+    .get("/api/v1/users")
+    .then(res => {
+      dispatch({type: GET_USERS_SUCCESS, payload: res.data})
+    })
+    .catch(err => {
+      dispatch({type: GET_USERS_SUCCESS, error: err})
+    })
+ }
+
+ export const GET_ALL_PROJECTS_START = 'GET_ALL_PROJECTS_START',
+ export const GET_ALL_PROJECTS_SUCCESS = 'GET_ALL_PROJECTS_SUCCESS',
+ export const GET_ALL_PROJECTS_FAILURE = 'GET_ALL_PROJECTS_FAILURE',
+
+
+
+ export const ADD_PROJECT_START = 'ADD_PROJECT_START',
+ export const ADD_PROJECT_SUCCESS = 'ADD_PROJECT_SUCCESS',
+ export const ADD_PROJECT_FAILURE = 'ADD_PROJECT_FAILURE',
+
+
+ export const UPDATE_USER_START = 'UPDATE_USER_START',
+ export const UPDATE_USER_SUCCESS = 'UPDATE_USER_SUCCESS',
+ export const UPDATE_USER_FAILURE = 'UPDATE_USER_FAILURE'
