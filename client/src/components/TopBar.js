1420982c7f139f197c61beb97e80a7ad52339baf
Mike VanSleen <mike.vansleen@gmail.com>
2019-09-13 12:32:49 -0600

    Created TopBar component and matching SCSS file

---
+++ client/src/components/TopBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SASS/TopBar.scss';
+
+const TopBar = () => {
+  return (
+    <div className="top-bar-container">
+      <h3>I'm the top bar, good day</h3>
+    </div>
+  );
+};
+
+export default TopBar;
