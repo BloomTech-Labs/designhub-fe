584c7d501efa112fcfec48536cd76468606609dd
Mike VanSleen <mike.vansleen@gmail.com>
2019-09-13 14:03:40 -0600

    Scaffolded out top bar components

---
+++ client/src/components/SearchBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchBar = () => {
+  return <input type="text" placeholder="search projects, users, teams..." />;
+};
+
+export default SearchBar;
--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import SampleLogo from './Icons/SampleLogo.js';
+import MagnifyingGlass from './Icons/MagnifyingGlass.js';
+import SearchBar from './SearchBar.js';
+import DarkModeSwitch from './Icons/DarkModeSwitch.js';
+import avatar from './ASSETS/avatar.jpg';
 import './SASS/TopBar.scss';
 
 const TopBar = () => {
+  const userInfo = {
+    avatar: avatar,
+    userName: 'eriklambert',
+    firstName: 'Erik',
+    lastName: 'Lambert',
+    bio:
+      'I love designing and always looking for ways to improve and innovate. 🤓 #Usersmatter! #LambdaBound #UXEngineer',
+    location: 'Austin, TX',
+    website: 'https://eriklambert.io',
+    projects: 12,
+    followers: 36,
+    following: 1,
+    starred: 143
+  };
+
   return (
     <div className="top-bar-container">
-      <h3>I'm the top bar, good day</h3>
+      <div className="logo-container">
+        <SampleLogo />
+      </div>
+      <div className="search-bar-container">
+        <MagnifyingGlass />
+        <SearchBar />
+      </div>
+      <div className="top-bar-user-info">
+        <p>{userInfo.userName}</p>
+        <img
+          className="profile-pic-thumb"
+          src={userInfo.avatar}
+          alt="user avatar"
+        />
+        <DarkModeSwitch />
+      </div>
     </div>
   );
 };
