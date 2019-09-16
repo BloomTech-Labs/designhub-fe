df33cd6e7e9421e8471f25982c07806cb7290efd
Mike VanSleen <mike.vansleen@gmail.com>
2019-09-16 13:10:55 -0600

    Started project component, linked SASS

---
+++ client/src/components/Project.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './SASS/Project.scss';
+
+const Project = () => {
+  return (
+    <div className="project-container">
+      <h3>Project goes here!</h3>
+    </div>
+  );
+};
+
+export default Project;
