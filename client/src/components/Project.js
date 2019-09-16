f6995d4f9014e4603acf523b0513530775afa257
Mike VanSleen <mike.vansleen@gmail.com>
2019-09-16 13:32:59 -0600

    Project header scaffolded

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -5,7 +5,26 @@ import './SASS/Project.scss';
 const Project = () => {
   return (
     <div className="project-container">
-      <h3>Project goes here!</h3>
+      <div className="project-header">
+        <div className="project-header-description">
+          <h2>DesignHub - Webdesign - Project</h2>
+          <h3>
+            DesignHub Product design files + Case study + assets + prototypes.
+          </h3>
+          <p>
+            <span>Created by eriklambert</span>
+            <span>Created on June 11, 2020</span>
+          </p>
+        </div>
+        <div className="project-header-right">
+          <div className="project-header-team">
+            Team members avatars go here
+          </div>
+          <div className="project-header-links">
+            download, star and other links here
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
