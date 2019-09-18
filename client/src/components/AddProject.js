ae3385b45f09f06e998c2c4b9b2500384823daf2
Mike VanSleen <mike.vansleen@gmail.com>
2019-09-18 13:30:23 -0600

    Getting user id from auth0 wrapper

--- client/src/components/AddProject.js
+++ client/src/components/AddProject.js
@@ -1,16 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { addProject } from '../store/actions';
+import { useAuth0 } from '../auth-wrapper.js';
 
 import '../SASS/Project.scss';
 
-const AddProject = () => {
+const AddProject = ({ addProject }) => {
+  const { user } = useAuth0();
+
+  const [project, setProject] = useState({
+    userId: user.id,
+    projectName: ''
+  });
+
+  const { userId, projectName } = project;
+
+  const handleChanges = e => {
+    setProject({
+      ...project,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    addProject(project).then(res => setProject({ projectName: '' }));
+  };
+
   return (
     <div style={{ marginTop: '78px' }}>
-      <form>
-        <input type="number" placeholder="user ID" />
-        <input type="text" placeholder="Project Name" />
-        <button>Add Project</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={projectName}
+          name="projectName"
+          placeholder="Project Name"
+          onChange={handleChanges}
+        />
+        <button type="submit">Add Project</button>
       </form>
     </div>
   );
