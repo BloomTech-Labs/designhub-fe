4c6efb3376847c4393da6992f8ce73283aed45f9
Mike VanSleen <mike.vansleen@gmail.com>
2019-09-18 14:11:49 -0600

    Form scaffolded out

--- client/src/components/AddProject.js
+++ client/src/components/AddProject.js
@@ -30,14 +30,52 @@ const AddProject = ({ addProject }) => {
   return (
     <div style={{ marginTop: '78px' }}>
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={projectName}
-          name="projectName"
-          placeholder="Project Name"
-          onChange={handleChanges}
-        />
-        <button type="submit">Add Project</button>
+        <div>
+          <label for="projectName">Project title</label>
+          <input
+            type="text"
+            value={projectName}
+            name="projectName"
+            id="projectName"
+            placeholder="Enter project title here"
+            onChange={handleChanges}
+          />
+          <label for="projectDescription">Project description</label>
+          <input
+            id="projectDescription"
+            type="text"
+            placeholder="Enter project description here"
+          />
+          <label for="teamMembers">Add team members</label>
+          <input
+            type="text"
+            placeholder="Enter team member usernames seperated by a comma"
+            id="teamMembers"
+          />
+        </div>
+
+        <div>
+          <h3>Attach files</h3>
+          <button type="submit">Attach files</button>
+
+          <h3>Included files</h3>
+          <p>File names will populate here</p>
+          <label for="figmaLink">Figma link</label>
+          <input
+            type="text"
+            placeholder="Enter Figma link here"
+            id="figmaLink"
+          />
+          <label for="invisionLink">Figma link</label>
+          <input
+            type="text"
+            placeholder="Enter InVision link here"
+            id="invisionLink"
+          />
+        </div>
+
+        <button type="submit">Publish</button>
+        <button type="button">Cancel</button>
       </form>
     </div>
   );
