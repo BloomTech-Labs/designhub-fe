42f4076024f1dc17a6c5e27cf80c1208eb098611
cecil john tantay <cjbtantay@gmail.com>
2019-09-18 17:22:10 -0700

    add test suite

---
+++ client/src/__tests__/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+afterEach(rtl.cleanup);
+
+it('renders without crashing', () => {
+  // ready to test!
+});
