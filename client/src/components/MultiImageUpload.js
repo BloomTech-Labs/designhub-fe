0cde4c336da0991f602c2e15fe09541d9130829f
cacheUp <bradb26@gmail.com>
2019-09-25 14:23:08 +0300

    delete useEffect comments

--- client/src/components/MultiImageUpload.js
+++ client/src/components/MultiImageUpload.js
@@ -73,14 +73,6 @@ export function MultiImageUpload(props) {
     ));
   };
 
-  // useEffect(
-  //   () => () => {
-  //     // Make sure to revoke the data uris to avoid memory leaks
-  //     files.forEach(file => URL.revokeObjectURL(file.preview));
-  //   },
-  //   [files]
-  // );
-
   return (
     <section className="container">
       <div
