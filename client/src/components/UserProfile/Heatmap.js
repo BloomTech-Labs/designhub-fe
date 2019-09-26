eff124d402224eee6f38cac41aef3a0485ccd619
cacheUp <bradb26@gmail.com>
2019-09-26 10:21:25 +0300

    shift date function

--- client/src/components/UserProfile/Heatmap.js
+++ client/src/components/UserProfile/Heatmap.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CalendarHeatmap from 'react-calendar-heatmap';
+import axios from 'axios';
 import 'react-calendar-heatmap/dist/styles.css';
 
 import '../../SASS/Heatmap.scss';
 
 const Heatmap = () => {
+  const [today, setToday] = useState(new Date());
+
+  function shiftDate(date, numDays) {
+    const newDate = new Date(date);
+    newDate.setDate(newDate.getDate() + numDays);
+    return newDate;
+  }
+
+  const getHeatmap = id => {};
+
   return (
     <div className="heatmap">
       <h1 className="header">Activity</h1>
       <CalendarHeatmap
-        startDate={new Date('2019-01-01')}
-        endDate={new Date('2019-12-31')}
+        startDate={shiftDate(today, -150)}
+        endDate={today}
         values={[
           { date: '2019-09-16', count: 1 },
           { date: '2019-09-17', count: 5 },
