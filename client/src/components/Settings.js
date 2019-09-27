928fa788f3c36d842dff941278f263052122074e
Javontay McElroy <dinomightnetwork@gmail.com>
2019-09-26 21:01:51 -0500

    Style nav and settings

--- client/src/components/Settings.js
+++ client/src/components/Settings.js
@@ -8,6 +8,10 @@ const Settings = ({ activeUser }) => {
   return (
     <div className="Settings">
       <img src={activeUser.avatar} alt="avatar" />
+      <h1 className="full-name">
+        {activeUser.firstName} {activeUser.lastName}
+      </h1>
+      <h1 className="email">{activeUser.email}</h1>
       <button className="logout-btn" onClick={() => logout()}>
         Log Out
       </button>
