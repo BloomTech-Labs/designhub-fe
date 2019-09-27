288bef4e1dbad99a6b0f261d52b8a0edbfb3a281
Javontay McElroy <dinomightnetwork@gmail.com>
2019-09-27 18:36:29 -0500

    removed bodymovin

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import bodymovin from 'bodymovin';
-
-import loading from '../ASSETS/loading.json';
-
 import '../SASS/Loading.scss';
 
 const Loading = () => {
-  var svgContainer = document.getElementById('svgContainer');
-  var animItem = bodymovin.loadAnimation({
-    wrapper: svgContainer,
-    animType: 'svg',
-    loop: true,
-    path: '../ASSETS/loading.json'
-  });
   return (
     <div className="Loading-container">
-      <div id="svgContainer"></div>
       <p className="loading-text">Loading awesome content...</p>
     </div>
   );
