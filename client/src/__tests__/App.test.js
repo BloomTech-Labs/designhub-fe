fde0e59d67c59cdf49fe1d6ad04c1ab53aaf127e
cacheUp <bradb26@gmail.com>
2019-09-30 13:40:38 +0300

    jest init, puppeteer init, login simulation

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import * as rtl from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+const puppeteer = require('puppeteer');
+jest.setTimeout(30000);
 
-afterEach(rtl.cleanup);
+test('renders without crashing', () => {
+  const condition = 1 + 2;
+  expect(condition).toEqual(3);
+});
 
-it('renders without crashing', () => {
-  // ready to test!
+test('login with auth0', async () => {
+  const browser = await puppeteer.launch({
+    headless: false
+  });
+  const page = await browser.newPage();
+  await page.goto('http://localhost:3000');
+  await page.click('.auth0-redirect-btn');
+
+  await page.waitForSelector('input[name="email"]', {
+    visible: true,
+    timeout: 5000
+  });
+  await page.type('input[name="email"]', 'test@test.com', { delay: 50 });
+  await page.type('input[name="password"]', 'Test1234', { delay: 50 });
+
+  await page.click('button[type="submit"]');
+  await page.waitFor('.edit-profile-btn');
+  const text = await page.$eval('.edit-profile-btn', el => el.innerHTML);
+  expect(text).toEqual('Edit Profile');
 });
