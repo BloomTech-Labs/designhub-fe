17ade91fbe6fb3ce5fbbb50e5c15ba03d27caec8
cacheUp <bradb26@gmail.com>
2019-09-30 17:55:33 +0300

    travis test

--- client/src/___test__helpers/page.js
+++ client/src/___test__helpers/page.js
@@ -2,7 +2,10 @@ const puppeteer = require('puppeteer');
 
 class CustomPage {
   static async build() {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox']
+    });
     const page = await browser.newPage();
     const customPage = new CustomPage(page);
 
