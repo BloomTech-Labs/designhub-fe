0808b960a040948bae20382986c3d6f0f2183e4e
cacheUp <bradb26@gmail.com>
2019-10-01 16:00:59 +0300

    tests creating a project

--- client/src/__tests__/projectForm.test.js
+++ client/src/__tests__/projectForm.test.js
@@ -7,8 +7,33 @@ beforeEach(async () => {
   page = await Page.build();
   await page.goto('http://localhost:3000');
   await page.login();
+  await page.goto('http://localhost:3000/create');
+  await page.waitFor('.page-header');
 });
 
 afterEach(async () => {
   await page.close();
 });
+
+describe('When logged in ', () => {
+  describe('And after going to /create url', () => {
+    test('Can submit a project', async () => {
+      await page.type('input[name="name"]', 'a beautiful project', {
+        delay: 50
+      });
+      await page.click('button[type="submit"]');
+      await page.waitFor('.project-details h2');
+      const text = await page.$eval('.project-details h2', el => el.innerHTML);
+      expect(text).toEqual('a beautiful project');
+    });
+  });
+});
+
+// test('test', async () => {
+//   //   await page.click('a[href="/create"]');
+//   //   //   console.log(projectLink);
+//   //   //   await page.click(projectLink);
+//   //
+//   const text = await page.$eval('.page-header', el => el.innerHTML);
+//   expect(text).toEqual('Create a project');
+// });
