8198fd04bc9e804048ead49c6282bea5dcce2c03
Mike VanSleen <mike.vansleen@gmail.com>
2019-10-02 15:37:11 -0600

    Settings component scaffolded

--- client/src/components/Settings.js
+++ client/src/components/Settings.js
@@ -7,14 +7,26 @@ const Settings = ({ activeUser }) => {
   const { logout } = useAuth0();
   return (
     <div className="Settings">
-      <img src={activeUser.avatar} alt="avatar" />
-      <h1 className="full-name">
-        {activeUser.firstName} {activeUser.lastName}
-      </h1>
-      <h1 className="email">{activeUser.email}</h1>
-      <button className="logout-btn" onClick={() => logout()}>
-        Log Out
-      </button>
+      <div className="settings-left-column">
+        <img src={activeUser.avatar} alt="avatar" />
+        <h4>Account</h4>
+        <h4>Privacy</h4>
+        <h4>Notifications</h4>
+        <h4>Linked Profiles</h4>
+        <h4>Download Data</h4>
+        <h4 className="settings-logout" onClick={() => logout()}>
+          Log Out
+        </h4>
+      </div>
+      <div className="settings-right-column">
+        <h1 className="full-name">
+          {activeUser.firstName} {activeUser.lastName}
+        </h1>
+        <h1 className="email">{activeUser.email}</h1>
+        <button className="logout-btn" onClick={() => logout()}>
+          Log Out
+        </button>
+      </div>
     </div>
   );
 };
