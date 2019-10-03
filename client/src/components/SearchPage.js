309abff8f09cdd10ab49005771202366d76c49cd
cacheUp <bradb26@gmail.com>
2019-10-03 15:52:02 +0300

    functionality done, just needs styling

--- client/src/components/SearchPage.js
+++ client/src/components/SearchPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 class SearchPage extends Component {
   constructor(props) {
@@ -6,24 +7,37 @@ class SearchPage extends Component {
   }
 
   displayUsers = () => {
-    this.props.users.map((user, index) => {
+    return this.props.searchData.users.map((user, index) => {
       return (
-        <div key={index}>
-          <h3>{user.username}</h3>
-          <p>{user.email}</p>
-          <img src={user.avatar} alt="avatar" />
-          <p>{user.bio}</p>
+        <div
+          key={index}
+          style={{
+            color: 'white'
+          }}
+        >
+          <h3 style={{ color: 'white' }}>{user.username}</h3>
+          <p style={{ color: 'white' }}>{user.email}</p>
+          <img
+            src={user.avatar}
+            alt="avatar"
+            style={{ height: '200px', width: '200px' }}
+          />
+          <p style={{ color: 'white', width: '350px' }}>{user.bio}</p>
         </div>
       );
     });
   };
 
   displayProjects = () => {
-    this.props.projects.map((project, index) => {
+    return this.props.searchData.projects.map((project, index) => {
       return (
         <div key={index}>
-          <h3>{project.name}</h3>
-          <img src={project.mainImg} alt="project" />
+          <h3 style={{ color: 'white' }}>{project.name}</h3>
+          <img
+            src={project.mainImg}
+            alt="project"
+            style={{ height: '30%', width: '30%' }}
+          />
         </div>
       );
     });
@@ -33,13 +47,39 @@ class SearchPage extends Component {
     console.log(this.props);
     return (
       <div>
-        <h2> HHHHHHHHEEEEEEEY</h2>
-        <h2> HHHHHHHHEEEEEEEY</h2>
-        <h2> HHHHHHHHEEEEEEEY</h2>
-        <h2> HHHHHHHHEEEEEEEY</h2>
-        <h2> HHHHHHHHEEEEEEEY</h2>
-        <h2> HHHHHHHHEEEEEEEY</h2>
-        <h2> HHHHHHHHEEEEEEEY</h2>
+        <Tabs style={{ marginTop: '100px' }}>
+          <TabList className="nav-links">
+            <Tab
+              className="links"
+              selectedClassName="active-link"
+              style={{ color: 'white' }}
+            >
+              Users
+            </Tab>
+            <Tab
+              className="links"
+              selectedClassName="active-link"
+              style={{ color: 'white' }}
+            >
+              Projects
+            </Tab>
+          </TabList>
+          <TabPanel
+            className="tabs-container"
+            style={{
+              border: '1px solid white',
+              display: 'flex',
+              justifyContent: 'space-between',
+              padding: '10px',
+              flexWrap: 'wrap'
+            }}
+          >
+            {this.props.searchData.users && this.displayUsers()}
+          </TabPanel>
+          <TabPanel className="tabs-container">
+            {this.props.searchData.projects && this.displayProjects()}
+          </TabPanel>
+        </Tabs>
       </div>
     );
   }
