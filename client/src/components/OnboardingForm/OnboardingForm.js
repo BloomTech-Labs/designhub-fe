322d43cb4303b76014e3fdcde35d48eb1c5004df
cecil john tantay <cjbtantay@gmail.com>
2019-10-07 14:01:03 -0700

    remove useEffect

--- client/src/components/OnboardingForm/OnboardingForm.js
+++ client/src/components/OnboardingForm/OnboardingForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import axios from 'axios';
 import { axiosWithAuth } from '../../utilities/axiosWithAuth.js';
