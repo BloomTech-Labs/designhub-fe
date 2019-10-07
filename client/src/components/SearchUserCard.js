c63cff66022b383e7a87da78727f60917f37c14a
Mike VanSleen <mike.vansleen@gmail.com>
2019-10-07 16:19:53 -0600

    Fixed link styling, each user card links to profile

--- client/src/components/SearchUserCard.js
+++ client/src/components/SearchUserCard.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SearchUserCard = ({ user }) => {
   return (
     <div className="search-user-card-container">
-      <div className="user-card-avatar">
-        {' '}
-        <img src={user.avatar} alt="avatar" />
-      </div>
-      <div className="user-card-info">
-        <h3>{`${user.firstName} ${user.lastName}`}</h3>
-        <p>@{user.username}</p>
-      </div>
+      <Link to={`/profile/${user.id}/${user.username}`}>
+        <div className="user-card-avatar">
+          {' '}
+          <img src={user.avatar} alt="avatar" />
+        </div>
+      </Link>
+      <Link to={`/profile/${user.id}/${user.username}`}>
+        <div className="user-card-info">
+          <h3>{`${user.firstName} ${user.lastName}`}</h3>
+          <p>@{user.username}</p>
+        </div>
+      </Link>
     </div>
   );
 };
