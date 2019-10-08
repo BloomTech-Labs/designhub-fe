da4cebe95fecd24fa5a0facb41a757b3fa9ba47d
Mike VanSleen <mike.vansleen@gmail.com>
2019-10-08 12:58:21 -0600

    Comments reducer/actions complete

--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -1,5 +1,6 @@
 import { axiosWithAuth } from '../../utilities/axiosWithAuth';
 
+//Users
 export const GET_ALL_USERS_START = 'GET_USERS_START';
 export const GET_ALL_USERS_SUCCESS = 'GET_USERS_SUCCESS';
 export const GET_ALL_USERS_FAILURE = 'GET_USERS_FAILURE';
@@ -9,6 +10,8 @@ export const GET_SINGLE_USER_FAILURE = 'GET_SINGLE_USER_FAILURE';
 export const UPDATE_USER_START = 'UPDATE_USER_START';
 export const UPDATE_USER_SUCCESS = 'UPDATE_USER_SUCCESS';
 export const UPDATE_USER_FAILURE = 'UPDATE_USER_FAILURE';
+
+// Projects
 export const GET_ALL_PROJECTS_START = 'GET_ALL_PROJECTS_START';
 export const GET_ALL_PROJECTS_SUCCESS = 'GET_ALL_PROJECTS_SUCCESS';
 export const GET_ALL_PROJECTS_FAILURE = 'GET_ALL_PROJECTS_FAILURE';
@@ -28,8 +31,27 @@ export const GET_USERS_PROJECTS_START = 'GET_USERS_PROJECTS_START';
 export const GET_USERS_PROJECTS_SUCCESS = 'GET_USERS_PROJECTS_SUCCESS';
 export const GET_USERS_PROJECTS_FAILURE = 'GET_USERS_PROJECTS_FAILURE';
 
-// Users actions
+//Comments
+export const GET_PROJECT_COMMENTS_START = 'GET_PROJECT_COMMENTS_START';
+export const GET_PROJECT_COMMENTS_SUCCESS = 'GET_PROJECT_COMMENTS_SUCCESS';
+export const GET_PROJECT_COMMENTS_FAILURE = 'GET_PROJECT_COMMENTS_FAILURE';
+export const GET_PHOTO_COMMENTS_START = 'GET_PHOTO_COMMENTS_START';
+export const GET_PHOTO_COMMENTS_SUCCESS = 'GET_PHOTO_COMMENTS_SUCCESS';
+export const GET_PHOTO_COMMENTS_FAILURE = 'GET_PHOTO_COMMENTS_FAILURE';
+export const ADD_PROJECT_COMMENT_START = 'ADD_PROJECT_COMMENT_START';
+export const ADD_PROJECT_COMMENT_SUCCESS = 'ADD_PROJECT_COMMENT_SUCCESS';
+export const ADD_PROJECT_COMMENT_FAILURE = 'ADD_PROJECT_COMMENT_FAILURE';
+export const ADD_PHOTO_COMMENT_START = 'ADD_PHOTO_COMMENT_START';
+export const ADD_PHOTO_COMMENT_SUCCESS = 'ADD_PHOTO_COMMENT_SUCCESS';
+export const ADD_PHOTO_COMMENT_FAILURE = 'ADD_PHOTO_COMMENT_FAILURE';
+export const UPDATE_COMMENT_START = 'UPDATE_COMMENT_START';
+export const UPDATE_COMMENT_SUCCESS = 'UPDATE_COMMENT_SUCCESS';
+export const UPDATE_COMMENT_FAILURE = 'UPDATE_COMMENT_FAILURE';
+export const DELETE_COMMENT_START = 'DELETE_COMMENT_START';
+export const DELETE_COMMENT_SUCCESS = 'DELETE_COMMENT_SUCCESS';
+export const DELETE_COMMENT_FAILURE = 'DELETE_COMMENT_FAILURE';
 
+// Users Actions
 export const getAllUsers = () => dispatch => {
   dispatch({ type: GET_ALL_USERS_START });
   return axiosWithAuth()
@@ -66,7 +88,7 @@ export const updateUser = (id, changes) => dispatch => {
     });
 };
 
-// Projects actions
+// Projects Actions
 export const getAllProjects = () => dispatch => {
   dispatch({ type: GET_ALL_PROJECTS_START });
   return axiosWithAuth()
@@ -136,5 +158,101 @@ export const getProjectsByUser = id => dispatch => {
     })
     .catch(err => {
       dispatch({ type: GET_USERS_PROJECTS_FAILURE, payload: err });
+    });
+};
+
+//Comments Actions
+export const getProjectComments = projectId => dispatch => {
+  dispatch({ type: GET_PROJECT_COMMENTS_START });
+  return axiosWithAuth()
+    .get(`/api/v1/comments/project/${projectId}`)
+    .then(res => {
+      dispatch({ type: GET_PROJECT_COMMENTS_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: GET_PROJECT_COMMENTS_FAILURE, payload: err });
+    });
+};
+
+export const getPhotoComments = photoId => dispatch => {
+  dispatch({ type: GET_PHOTO_COMMENTS_START });
+  return axiosWithAuth()
+    .get(`/api/v1/comments/photo/${photoId}`)
+    .then(res => {
+      dispatch({ type: GET_PHOTO_COMMENTS_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: GET_PHOTO_COMMENTS_FAILURE, payload: err });
+    });
+};
+
+export const addProjectComment = comment => dispatch => {
+  dispatch({ type: ADD_PROJECT_COMMENT_START });
+  return axiosWithAuth()
+    .post('/api/v1/comments/project', comment)
+    .then(res => {
+      dispatch({
+        type: ADD_PROJECT_COMMENT_SUCCESS,
+        payload: res.data.data[0]
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: ADD_PROJECT_COMMENT_FAILURE,
+        payload: err.data.message
+      });
+    });
+};
+
+export const addPhotoComment = comment => dispatch => {
+  dispatch({ type: ADD_PHOTO_COMMENT_START });
+  return axiosWithAuth()
+    .post('/api/v1/comments/photo', comment)
+    .then(res => {
+      dispatch({
+        type: ADD_PHOTO_COMMENT_SUCCESS,
+        payload: res.data.data[0]
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: ADD_PHOTO_COMMENT_FAILURE,
+        payload: err.data.message
+      });
+    });
+};
+
+export const updateComment = (changes, id) => dispatch => {
+  dispatch({ type: UPDATE_COMMENT_START });
+  return axiosWithAuth()
+    .put(`/api/v1/comments/${id}`, changes)
+    .then(res => {
+      dispatch({
+        type: UPDATE_COMMENT_SUCCESS,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: UPDATE_COMMENT_FAILURE,
+        payload: err.data.message
+      });
+    });
+};
+
+export const deleteComment = id => dispatch => {
+  dispatch({ type: DELETE_COMMENT_START });
+  return axiosWithAuth()
+    .delete(`/api/v1/comments/${id}`)
+    .then(res => {
+      dispatch({
+        type: DELETE_COMMENT_SUCCESS
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: DELETE_COMMENT_FAILURE,
+        payload: err.data.message
+      });
     });
 };
