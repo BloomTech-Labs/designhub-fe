5f680be64cd8fbe4b0cea26f74ef265084f29d59
cacheUp <bradb26@gmail.com>
2019-10-10 16:14:50 +0300

    mounted notifications to state

--- client/src/components/Notifications.js
+++ client/src/components/Notifications.js
@@ -1,15 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { axiosWithAuth } from '../utilities/axiosWithAuth';
 
 const Notifications = props => {
+  const [state, setState] = useState([]);
   const { id } = props.activeUser;
-  console.log(id);
-  const getNotifications = id => {
-    axiosWithAuth().get(`api/v1/invite/${id}`);
-  };
 
-  console.log(props);
-  return <div>Notifications</div>;
+  useEffect(() => {
+    try {
+      const getNotifications = async id => {
+        const { data } = await axiosWithAuth().get(`api/v1/invite/${id}`);
+        setState(data);
+      };
+      getNotifications(id);
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
+
+  const renderBasedOnType = item => {};
+
+  const renderUnread = array => {};
+
+  const renderRead = array => {};
+
+  console.log(state);
+  return <div>{JSON.stringify(state)}</div>;
 };
 
 export default Notifications;
