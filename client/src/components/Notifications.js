bcd9dc5f67302fc05cc49c64cfb3a6043d3d33d4
cacheUp <bradb26@gmail.com>
2019-10-10 16:46:54 +0300

    little styling

--- client/src/components/Notifications.js
+++ client/src/components/Notifications.js
@@ -19,29 +19,72 @@ const Notifications = props => {
   }, []);
 
   const renderBasedOnType = item => {
+    console.log(item.unread);
     if (item.type === 'comment') {
       return (
-        <div>
-          {item.unRead === true && <h2>UNREAD</h2>}
-          <img src={item.activeUserAvatar} />
+        <div
+          style={{
+            marginBotton: '20px',
+            marginTop: '20px',
+            display: 'flex',
+            width: '500px'
+          }}
+        >
+          {item.unread === true ? <h2>UNREAD</h2> : <h2>READ</h2>}
+          <img
+            style={{ width: '120px', height: '120px' }}
+            src={item.activeUserAvatar}
+          />
           <p>
-            <span>{item.activeUsername} commented</span>
+            <span style={{ marginRight: '10px' }}>
+              {item.activeUsername} commented
+            </span>{' '}
             {item.commentText}
           </p>
           <p>{moment(item.created_at).fromNow()} </p>
-          <img src={item.mainImgUrl} />
+          <img
+            style={{ width: '120px', height: '120px' }}
+            src={item.mainImgUrl}
+          />
         </div>
       );
     } else if (item.type === 'follow') {
+      return (
+        <div
+          style={{
+            marginBotton: '20px',
+            marginTop: '20px',
+            display: 'flex',
+            width: '500px'
+          }}
+        >
+          {item.unread === true && <h2>UNREAD</h2>}
+          <img
+            style={{ width: '120px', height: '120px' }}
+            src={item.activeUserAvatar}
+          />
+          <p>
+            <span>{item.activeUsername} followed</span>
+          </p>
+          <p>{'  '}you</p>
+          <p> {moment(item.created_at).fromNow()} </p>
+        </div>
+      );
     }
   };
 
-  const renderUnread = array => {};
+  const renderUnread = array => {
+    return array.map(i => renderBasedOnType(i));
+  };
 
   const renderRead = array => {};
 
   console.log(state);
-  return <div>{'notifications'}</div>;
+  return (
+    <div>
+      {state.unReadNotifications && renderUnread(state.unReadNotifications)}
+    </div>
+  );
 };
 
 export default Notifications;
