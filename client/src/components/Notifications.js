39774a81a6a6d5024b83e90c97a4159695e2e02a
cacheUp <bradb26@gmail.com>
2019-10-10 15:57:03 +0300

    get notifications

--- client/src/components/Notifications.js
+++ client/src/components/Notifications.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { axiosWithAuth } from '../utilities/axiosWithAuth';
 
 const Notifications = props => {
+  const { id } = props.activeUser;
+  console.log(id);
+  const getNotifications = id => {
+    axiosWithAuth().get(`api/v1/invite/${id}`);
+  };
+
+  console.log(props);
   return <div>Notifications</div>;
 };
 
