9c0daf36c786470fefab5286a7ad767865baab42
cecil john tantay <cjbtantay@gmail.com>
2019-10-16 02:29:12 -0700

    fix module

--- client/src/__tests__/NavBar.test.js
+++ client/src/__tests__/NavBar.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import NavBar from '../components/NavBar'
+import NavBar from '../components/Navbar.js'
 
 import rendererWithRouter from '../__testHelpers__/rendererWithRouter'
 
