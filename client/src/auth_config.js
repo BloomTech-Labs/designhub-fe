ac517d022a313805674f93e6ec874d5fc1cbbfac
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2019-11-11 15:24:38 -0800

    Updated auth configuration

--- client/src/auth_config.js
+++ client/src/auth_config.js
@@ -1,7 +1,8 @@
 const config = {
   domain: `${process.env.REACT_APP_DOMAIN}`,
   clientId: `${process.env.REACT_APP_CLIENT_ID}`,
-  audience: `${process.env.REACT_APP_BASE_URL}`
+  // audience: `${process.env.REACT_APP_BASE_URL}`
+  audience: `https://designhubx-staging.herokuapp.com/`
 };
 
 export default config;
