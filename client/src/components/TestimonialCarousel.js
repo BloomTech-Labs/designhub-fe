3cacbe326ad168a5625893ecf00baf5c732a7fd2
Jenn Soderborg <jsoderborg7@gmail.com>
2019-12-02 14:35:34 -0700

    changed carousel component, working on carousel styling. Waiting for input from UX
    Co-authored-by: shanreed <srtech5set@gmail.com>

--- client/src/components/TestimonialCarousel.js
+++ client/src/components/TestimonialCarousel.js
@@ -1,50 +1,49 @@
-import React from "react";
-import { MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBView, MDBContainer } from
-"mdbreact";
+import React, {useState, useEffect} from 'react';
+import {Gallery, GalleryImage} from 'react-gesture-gallery';
 
-const TestimonialCarousel = () => {
-  return (
-    <MDBContainer>
-      <MDBCarousel
-        activeItem={1}
-        length={3}
-        showControls={true}
-        showIndicators={true}
-        className="z-depth-1"
-      >
-        <MDBCarouselInner>
-          <MDBCarouselItem itemId="1">
-            <MDBView>
-              <img
-                className="d-block w-100"
-                src="https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg"
-                alt="First slide"
-              />
-            </MDBView>
-          </MDBCarouselItem>
-          <MDBCarouselItem itemId="2">
-            <MDBView>
-              <img
-                className="d-block w-100"
-                src="https://mdbootstrap.com/img/Photos/Slides/img%20(129).jpg"
-                alt="Second slide"
-              />
-            </MDBView>
-          </MDBCarouselItem>
-          <MDBCarouselItem itemId="3">
-            <MDBView>
-              <img
-                className="d-block w-100"
-                src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"
-                alt="Third slide"
-              />
-            </MDBView>
-          </MDBCarouselItem>
-        </MDBCarouselInner>
-      </MDBCarousel>
-    </MDBContainer>
-  );
+import '../SASS/TestimonialCarousel.scss';
+
+
+const testimonials =[
+  "https://images.unsplash.com/photo-1567877163288-afa41ad24d8c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" ,
+
+  "https://images.unsplash.com/photo-1575015642299-5b92fcbd0ba4?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" ,
+
+  "https://images.unsplash.com/photo-1575126473812-9331d9d93530?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" ,
+
+  "https://images.unsplash.com/photo-1572270907014-c31da1c54124?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+];
+
+function TestimonialCarousel() {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() =>{
+    const timer = setInterval(() =>{
+      if (index === 3) {
+        setIndex(0);
+      } else {
+        setIndex(prev => prev + 1);
+      }
+    }, 5000);
+    return () => clearInterval(timer);
+  }, [index]);
+
+  return(
+    <div className="carousel-container">
+      <Gallery
+        enableIndicators={true}
+        enableControls={true}
+        index={index}
+        onRequestChange={i =>{
+          setIndex(i);
+        }}
+        >
+          {testimonials.map(testimonial =>(
+            <GalleryImage objectFit="contain" key={testimonial} src={testimonial} />
+          ))}
+        </Gallery>
+      </div>
+  )
 }
 
 export default TestimonialCarousel;
-
