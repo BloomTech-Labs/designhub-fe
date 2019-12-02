666707ac9f63d3b1e46a15818bd48bfd0f6c443c
Ahmad Guenoun <amguenoun@gmail.com>
2019-12-02 15:42:23 -0500

    preliminary styling and modal functionality built out
    Co-authored-by: tishaholder <tishay_ann@yahoo.com>
    Co-authored-by: rogermcconkiejr <roger.mcconkiejr@gmail.com>

--- client/src/components/ProjectForm.js
+++ client/src/components/ProjectForm.js
@@ -56,6 +56,7 @@ const ProjectForm = ({
     url: '',
     modal: false,
     deletingImage: null,
+    inviteModal: false,
     projectPhotos: null
   });
 
@@ -93,7 +94,7 @@ const ProjectForm = ({
       setError('Project title is required');
       titleRef.scrollIntoView({ behavior: 'smooth', block: 'center' });
       return;
-    } else if(!state.project.mainImg) {
+    } else if (!state.project.mainImg) {
       setIsLoading(false);
       setError('Please upload at least one image');
       return;
@@ -226,6 +227,13 @@ const ProjectForm = ({
     });
   };
 
+  const closeInviteModal = () => {
+    setState({
+      ...state,
+      inviteModal: false,
+    })
+  }
+
   const thumbInner = {
     display: 'flex',
     minWidth: 0,
@@ -234,153 +242,174 @@ const ProjectForm = ({
 
   return (
     isEditing && user.id !== project.userId ? <Redirect to={`/project/${project.id}`} /> :
-    <div className="project-form-wrapper">
-      {isLoading && <Loading />}
-      <div className={state.modal ? 'modal--expand' : 'modal--close'}>
-        <span
-          className="modal--expand__background-overlay"
-          onClick={closeModal}
-        >
-          {state.modal && (
-            <div className="delete-project-modal">
-              <p>Are you sure you want to delete that?</p>
-              <div className="delete-modal-button-container">
-                <button onClick={closeModal}>Cancel</button>
-                <button
-                  className="delete-button"
-                  onClick={() => {
-                    if (state.deletingImage) {
-                      handleDeletePhoto(state.deletingImage);
-                    } else {
-                      handleDeleteProject(project.id);
-                    }
-                  }}
-                >
-                  Delete
+      <div className="project-form-wrapper">
+        {isLoading && <Loading />}
+        <div className={state.modal ? 'modal--expand' : 'modal--close'}>
+          <span
+            className="modal--expand__background-overlay"
+            onClick={closeModal}
+          >
+            {state.modal && (
+              <div className="delete-project-modal">
+                <p>Are you sure you want to delete that?</p>
+                <div className="delete-modal-button-container">
+                  <button onClick={closeModal}>Cancel</button>
+                  <button
+                    className="delete-button"
+                    onClick={() => {
+                      if (state.deletingImage) {
+                        handleDeletePhoto(state.deletingImage);
+                      } else {
+                        handleDeleteProject(project.id);
+                      }
+                    }}
+                  >
+                    Delete
                 </button>
+                </div>
               </div>
-            </div>
-          )}
-        </span>
-      </div>
-      <section className="ProjectForm__body">
-        <div className="left-container">
-          <header className="ProjectForm__header">
-            <h2 className="page-header">
-              {isEditing ? 'Edit project' : 'Create a project'}
-            </h2>
-          </header>
-          <MultiImageUpload filesArray={{ files, setFiles }} />
-          {isEditing && (
-            <div>
-              <div className="thumbnail-container ">
-                {projectPhotos.map((photo, index) => (
-                  <div key={index}>
-                    <img
-                      alt=""
-                      src={remove}
-                      className="remove"
-                      onClick={e => {
-                        setState({
-                          ...state,
-                          deletingImage: photo.id,
-                          modal: true
-                        });
-                      }}
-                    />
-                    <div className="thumb" key={index}>
-                      <div style={thumbInner}>
-                        <img
-                          alt="project thumbnail"
-                          src={photo.url}
-                          className="thumbnail"
-                        />
+            )}
+          </span>
+        </div>
+        <div className="project-form-wrapper">
+          {isLoading && <Loading />}
+          <div className={state.inviteModal ? 'modal--expand' : 'modal--close'}>
+            <span
+              className="modal--expand__background-overlay"
+              onClick={closeInviteModal}
+            >
+              {state.inviteModal && (
+                <div>
+                  collab
+             </div>
+              )}
+            </span>
+          </div>
+        </div>
+        <section className="ProjectForm__body">
+          <div className="left-container">
+            <header className="ProjectForm__header">
+              <h2 className="page-header">
+                {isEditing ? 'Edit project' : 'Create a project'}
+              </h2>
+            </header>
+            <MultiImageUpload filesArray={{ files, setFiles }} />
+            {isEditing && (
+              <div>
+                <div className="thumbnail-container ">
+                  {projectPhotos.map((photo, index) => (
+                    <div key={index}>
+                      <img
+                        alt=""
+                        src={remove}
+                        className="remove"
+                        onClick={e => {
+                          setState({
+                            ...state,
+                            deletingImage: photo.id,
+                            modal: true
+                          });
+                        }}
+                      />
+                      <div className="thumb" key={index}>
+                        <div style={thumbInner}>
+                          <img
+                            alt="project thumbnail"
+                            src={photo.url}
+                            className="thumbnail"
+                          />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                ))}
+                  ))}
+                </div>
               </div>
-            </div>
-          )}
-        </div>
-        <div className="right-container">
-          <form
-            encType="multipart/form-data"
-            className="project-form-container"
-          >
-            <div className='required'>
-              <label htmlFor="name" className="label project-label">
-                Project title *
-              </label>
-              <input
-                required
-                autoFocus={true}
-                type="text"
-                value={name}
-                name="name"
-                id="name"
-                placeholder="Enter project title here"
-                onChange={handleChanges}
-                ref={setTitleRef}
-              />
-            </div>
-            <label htmlFor="description" className="label">
-              Project description
-            </label>
-            <textarea
-              id="description"
-              name="description"
-              value={description}
-              type="text"
-              placeholder="Enter description here"
-              onChange={handleChanges}
-              className="description"
-              maxLength="240"
-            />
-            <CharacterCount string={description} limit={240} />
-            <label htmlFor="figmaLink" className="label">
-              Figma
-            </label>
-            <input
-              type="text"
-              name="figma"
-              value={figma}
-              placeholder="Enter link here (optional)"
-              id="figmaLink"
-              onChange={handleChanges}
-            />
-            <label htmlFor="invisionLink" className="label">
-              Prototype
-            </label>
-            <input
-              type="text"
-              name="invision"
-              value={invision}
-              placeholder="Enter link here (optional)"
-              id="invisionLink"
-              onChange={handleChanges}
-            />
-            <label htmlFor="privacyLink" className="label">
-              Privacy
-            </label> {/*PROTOTYPE LABEL AND TEXT FIELD*/}
-            <select
-              type="select"
-              name="privacy"
-              value={privacy}
-              placeholder="Select privacy settings"
-              id="privacy"
-              onChange={handlePrivacySetting}
+            )}
+          </div>
+          <div className="right-container">
+            <form
+              encType="multipart/form-data"
+              className="project-form-container"
             >
-              {/*THERE IS A private BOOLEAN FIELD IN THE USER_PROJECTS TABLE: 1 is true or private and 0 is false or public.
+              <div className='required'>
+                <label htmlFor="name" className="label project-label">
+                  Project title *
+              </label>
+                <input
+                  required
+                  autoFocus={true}
+                  type="text"
+                  value={name}
+                  name="name"
+                  id="name"
+                  placeholder="Enter project title here"
+                  onChange={handleChanges}
+                  ref={setTitleRef}
+                />
+              </div>
+              <label htmlFor="description" className="label">
+                Project description
+            </label>
+              <textarea
+                id="description"
+                name="description"
+                value={description}
+                type="text"
+                placeholder="Enter description here"
+                onChange={handleChanges}
+                className="description"
+                maxLength="240"
+              />
+              <CharacterCount string={description} limit={240} />
+              <label htmlFor="figmaLink" className="label">
+                Figma
+            </label>
+              <input
+                type="text"
+                name="figma"
+                value={figma}
+                placeholder="Enter link here (optional)"
+                id="figmaLink"
+                onChange={handleChanges}
+              />
+              <label htmlFor="invisionLink" className="label">
+                Prototype
+            </label>
+              <input
+                type="text"
+                name="invision"
+                value={invision}
+                placeholder="Enter link here (optional)"
+                id="invisionLink"
+                onChange={handleChanges}
+              />
+              <label htmlFor="privacyLink" className="label">
+                Privacy
+            </label> {/*PROTOTYPE LABEL AND TEXT FIELD*/}
+              <select
+                type="select"
+                name="privacy"
+                value={privacy}
+                placeholder="Select privacy settings"
+                id="privacyLink"
+                onChange={handlePrivacySetting}
+              >
+                {/*THERE IS A private BOOLEAN FIELD IN THE USER_PROJECTS TABLE: 1 is true or private and 0 is false or public.
                IT DEFAULTS TO FALSE (0) or PUBLIC*/}
-              {/*TO SELECT ALL PUBLIC PROJECTS IN POSTGRES: SELECT * FROM USER_PROJECTS WHERE NOT PRIVATE*/}
-              {/*TO SELECT ALL PRIVATE PROJECTS IN POSTGRES: SELECT * FROM USER_PROJECTS WHERE PRIVATE = "YES"
+                {/*TO SELECT ALL PUBLIC PROJECTS IN POSTGRES: SELECT * FROM USER_PROJECTS WHERE NOT PRIVATE*/}
+                {/*TO SELECT ALL PRIVATE PROJECTS IN POSTGRES: SELECT * FROM USER_PROJECTS WHERE PRIVATE = "YES"
                 OR SELECT * FROM USER_PROJECTS WHERE PRIVATE */}
-              <option value="public">Public</option>
-              <option value="private">Private</option>
-            </select>
-            <p className="required-help">* Required</p>
-            {/* <label htmlFor="teamMembers" className="label">
+                <option value="public">Public</option>
+                <option value="private">Private</option>
+              </select>
+              <label htmlFor="inviteLink" className='label'>
+                Collaborators
+            </label>
+              <div id="inviteLink" className="invite" onClick={() => setState({ ...state, inviteModal: true })}>
+                <div>+</div>
+              </div>
+              <p className="required-help">* Required</p>
+              {/* <label htmlFor="teamMembers" className="label">
               Add team members
             </label>
             <input
@@ -388,47 +417,47 @@ const ProjectForm = ({
               placeholder="Enter team member usernames separated by a comma (optional)"
               id="teamMembers"
             /> */}
-            <div className="submit-cancel-container">
-              <button
-                type="button"
-                className="cancel-btn"
-                onClick={() => {
-                  history.goBack();
-                }}
-                disabled={isLoading}
-              >
-                Cancel
+              <div className="submit-cancel-container">
+                <button
+                  type="button"
+                  className="cancel-btn"
+                  onClick={() => {
+                    history.goBack();
+                  }}
+                  disabled={isLoading}
+                >
+                  Cancel
               </button>
-              <button
-                className="submit-button"
-                type="submit"
-                onClick={handleSubmit}
-                disabled={isLoading}
-              >
-                {isEditing ? 'Save Changes' : 'Publish'}
-              </button>
-            </div>
-            <div className='error'>
-              {error}
-            </div>
-            {isEditing && (
-              <div
-                className="delete-project-button"
-                onClick={() =>
-                  setState({
-                    ...state,
-                    modal: true
-                  })
-                }
-              >
-                <DeleteIcon />
-                <p>Delete project</p>
+                <button
+                  className="submit-button"
+                  type="submit"
+                  onClick={handleSubmit}
+                  disabled={isLoading}
+                >
+                  {isEditing ? 'Save Changes' : 'Publish'}
+                </button>
               </div>
-            )}
-          </form>
-        </div>
-      </section>
-    </div>
+              <div className='error'>
+                {error}
+              </div>
+              {isEditing && (
+                <div
+                  className="delete-project-button"
+                  onClick={() =>
+                    setState({
+                      ...state,
+                      modal: true
+                    })
+                  }
+                >
+                  <DeleteIcon />
+                  <p>Delete project</p>
+                </div>
+              )}
+            </form>
+          </div>
+        </section>
+      </div>
   );
 };
 export default withRouter(
