16173cb49844ad00c7ca8c36ee5206729304d110
Ahmad Guenoun <amguenoun@gmail.com>
2019-12-12 18:36:27 -0500

    preliminary styling done

--- client/src/components/ProjectThumbnail.js
+++ client/src/components/ProjectThumbnail.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 
 import defaultImg from '../ASSETS/default_thumbnail.svg';
 
+import '../SASS/ProjectThumbnail.scss';
+
 const ProjectThumbnail = ({ project, user }) => {
     return !project || !user ? null : (
         <div className="project-content">
@@ -11,17 +13,17 @@ const ProjectThumbnail = ({ project, user }) => {
                 <div className="project-info">
                     <div className='project-flex'>
                         <img className='avatar' src={user.avatar} alt={user.firstName} />
-                        <h1 className='project-username'>{user.username}</h1>
-                        <div>
+                        <div className="project-middle">
                             {project.name.length > 35 ? (
                                 <h1>{project.name.slice(0, 35)}...</h1>
                             ) : (
                                     <h1>{project.name}</h1>
                                 )}
-                            <h1 className="created">
-                                {moment(project.created_at).format('MMM DD, YYYY')}
-                            </h1>
+                            <h1 className='project-username'>{user.username}</h1>
                         </div>
+                        <h1 className="created">
+                            {moment(project.created_at).format('MMM DD, YYYY')}
+                        </h1>
                     </div>
                 </div>
                 <img
