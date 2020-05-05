8e66551317a230cfc2d122e6e063bb84051b0f81
andrewbastian <andrew.bastian@gmail.com>
2020-05-04 18:05:07 -0700

    Removed components folder

--- src/components/AddProject.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ProjectForm from './ProjectForm';
-
-const AddProject = ({ activeUser }) => {
-  return <ProjectForm user={activeUser} />;
-};
-
-export default AddProject;
--- src/components/Settings/Privacy.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Privacy = () => {
-  return <div>Privacy Tab</div>;
-};
-
-export default Privacy;
