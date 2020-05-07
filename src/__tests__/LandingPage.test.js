5ca8459923b8b6518d50ff801b43981e5753116c
andrewbastian <andrew.bastian@gmail.com>
2020-05-06 18:29:29 -0700

    Fixed broken unit tests

--- src/__tests__/LandingPage.test.js
+++ src/__tests__/LandingPage.test.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import LandingPage from '../components/LandingPage'
+import React from 'react';
+import LandingPage from '../views/LandingPage/LandingPage';
 
-import { useAuth0 } from '../utilities/auth-wrapper'
+import { useAuth0 } from '../utilities/auth-wrapper';
 
-import rendererWithRouter from '../__testHelpers__/rendererWithRouter'
+import rendererWithRouter from '../__testHelpers__/rendererWithRouter';
 
 const user = {
   email: 'johndoe@me.com',
   email_verified: true,
   sub: 'google-oauth2|2147627834623744883746',
-}
+};
 
-jest.mock('../auth-wrapper');
+jest.mock('../utilities/auth-wrapper');
 describe('<LandingPage />', () => {
   beforeEach(() => {
     useAuth0.mockReturnValue({
       user,
-      loginWithRedirect: jest.fn()
-    })
-  })
+      loginWithRedirect: jest.fn(),
+    });
+  });
 
   describe('should match the previous snapshot of the component', () => {
     it('tests that the component has not changed and matched the previous snapshot', () => {
@@ -28,4 +28,4 @@ describe('<LandingPage />', () => {
       expect(tree.toJSON()).toMatchSnapshot();
     });
   });
-})
+});
--- src/__tests__/ProjectForm.test.js
+++ src/__tests__/ProjectForm.test.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import ProjectForm from '../components/ProjectForm'
+import React from 'react';
+import ProjectForm from '../views/Projects/ProjectForm';
 
-import rendererWithRouter from '../__testHelpers__/rendererWithRouter'
+import rendererWithRouter from '../__testHelpers__/rendererWithRouter';
 
 describe('<ProjectForm />', () => {
-
   describe('should match the previous snapshot of the component', () => {
     it('tests that the component has not changed and matched the previous snapshot', () => {
       const activeUser = {
-        id: 1
-      }
+        id: 1,
+      };
       const tree = rendererWithRouter(
-        <ProjectForm user={activeUser} history={{ location: { pathname: '/' } }} />
+        <ProjectForm
+          user={activeUser}
+          history={{ location: { pathname: '/' } }}
+        />
       );
       expect(tree.toJSON()).toMatchSnapshot();
     });
   });
-})
+});
