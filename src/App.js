4ffdbad0026f0b339eff1eba76ee8dac67950df2
andrewbastian <andrew.bastian@gmail.com>
2020-06-14 14:33:54 -0700

    Index and App v1

--- src/App.js
+++ src/App.js
@@ -17,20 +17,18 @@ import Notifications from './views/Notifications';
 export default function App() {
   return (
     <>
-    
-    <div className="app-wrapper">
-      <main className="workspace">
-            
-        <Route path="/onboarding" component={Onboarding} />
-        <Route path="/profile/:username" component={Profile} />
-        <Route path="/project/:id" component={Project} />
-        <Route path="/search" component={Search} />
-        <Route exact path="/" component={Explore} />  
-        <PrivateRoute path="/notifications" component={Notifications} />
-        <PrivateRoute path="/create-project" component={CreateProject} />
-        <PrivateRoute path="/settings" component={Settings} />
-      </main>
-    </div>
+      <div className="app-wrapper">
+        <main className="workspace">
+          <Route exact path="/" component={Explore} />
+          <Route path="/onboarding" component={Onboarding} />
+          <Route path="/profile/:username" component={Profile} />
+          <Route path="/project/:id" component={Project} />
+          <Route path="/search" component={Search} />
+          <PrivateRoute path="/notifications" component={Notifications} />
+          <PrivateRoute path="/create-project" component={CreateProject} />
+          <PrivateRoute path="/settings" component={Settings} />
+        </main>
+      </div>
     </>
   );
 }
