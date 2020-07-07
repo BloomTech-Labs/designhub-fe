379acf43b54b6031385abeec01b4f185d6e0f340
brendantfinn <brendantfinn@gmail.com>
2020-07-07 13:54:50 -0500

    hardcoded profile with sass styling

--- src/views/Profile/index.js
+++ src/views/Profile/index.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import Layout from '../../common/Layout';
-import { useParams } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
 // import './styles.scss';
 
 export default function Profile() {
-  const { username, avatar} = useParams();
+  // const { username, avatar} = useParams();
   return (
     <Layout>
       <div className="user-profile-container">
         <div className="user-header">
           <div className="user-left-content">
-          <img
-              src={avatar}
+            <img
+              // src={avatar}
               className="avatar"
               alt="avatar"
             />
 
-<div className="user-info">
+            <div className="user-info">
               <h1 className="userFLname">Erik Lambert</h1>
               <h2 className="username">eriklambert</h2>
-              <p className="bio">An About section is the part of your website where you tell visitors and potential customers who you are and what you can do for them.</p>
+              <p className="bio">
+                An About section is the part of your website where you tell
+                visitors and potential customers who you are and what you can do
+                for them.
+              </p>
               <div className="user-info-location-website">
                 <div className="location-website-flex">
                   <p className="location">Austin, TX</p>
@@ -30,7 +34,10 @@ export default function Profile() {
                     href="www.eriklambert.com"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="website">eriklambert.com</a>
+                    className="website"
+                  >
+                    eriklambert.com
+                  </a>
                 </div>
               </div>
             </div>
@@ -43,7 +50,6 @@ export default function Profile() {
                 <p>12</p>
               </div>
 
-          
               <div className="count-flex">
                 <h6>Followers</h6>
                 <p>37</p>
@@ -62,16 +68,14 @@ export default function Profile() {
                 <h6>Teams</h6>
                 <p></p>
               </div>
-              
-              <div>
-                  <button className="edit-profile-btn">Edit Profile</button>
-              </div>
 
+              <div>
+                <button className="edit-profile-btn">Edit Profile</button>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </Layout>
-
   );
 }
