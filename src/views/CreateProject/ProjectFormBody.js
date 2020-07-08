0f2f74d7563bb149119ffbd9fcbda974f5cd6080
andrewbastian <andrew.bastian@gmail.com>
2020-07-08 15:48:09 -0700

    Renamed state and code-formatting.

--- src/views/CreateProject/ProjectFormBody.js
+++ src/views/CreateProject/ProjectFormBody.js
@@ -16,9 +16,8 @@ import {
   UPDATE_PROJECT_MUTATION,
   UPDATE_PROJECT_PHOTO_MUTATION,
   DELETE_PROJECT_PHOTO_MUTATION,
-  DELETE_PROJECT_MUTATION
+  DELETE_PROJECT_MUTATION,
 } from '../../graphql/index';
-
 
 import './styles.scss';
 
@@ -29,10 +28,10 @@ const ProjectFromBody = ({
   project,
   projectPhotos,
   userData,
-  user
+  user,
 }) => {
 
- console.log('PROJECT USER', userData)
+  console.log('PROJECT USER', userData);
 
   const [files, setFiles] = useState([]);
   const [researchFile, setResearchFile] = useState([]);
@@ -40,10 +39,9 @@ const ProjectFromBody = ({
   const [titleRef, setTitleRef] = useState(null);
   const [error, setError] = useState('');
   const [privacy, setPrivacy] = useState(
-
     isEditing ? (project.privateProjects ? 'private' : 'public') : 'public'
   );
-    const [newProject, setNewProject] = useState({
+  const [newProjectData, setNewProjectData] = useState({
     project: {
       userId: user?.sub,
       name: isEditing ? project?.name : '',
@@ -158,77 +156,82 @@ const ProjectFromBody = ({
 
   return (
     <>
-      <DeleteProjectModal modal={newProject.modal} deletingImage={newProject.deletingImage} deletingResearch={newProject.deletingResearch} />
-        <div className="project-form-wrapper">
-          <InviteModal />
-        </div>
-    <section className="ProjectForm__body">
-      <div className="left-container">
-        <header className="ProjectForm__header">
-          <h2 className="page-header"></h2>
-        </header>
-        <MultiImageUpload files={files} setFiles={setFiles} />
-        {isEditing && (
-          <div>
-            <div className="thumbnail-container ">
-              {projectPhotos.map((photo, index) => (
-                <div key={index}>
-                  <img
-                    alt=""
-                    src={remove}
-                    className="remove"
-                    onClick={(e) => {
-                      
-                    }}
-                  />
+      <DeleteProjectModal
+        closeModal={closeModal}
+        deletingImage={newProjectData.deletingImage}
+        deletingResearch={newProjectData.deletingResearch}
+      />
+      <div className="project-form-wrapper">
+        <InviteModal />
+      </div>
+      <section className="ProjectForm__body">
+        <div className="left-container">
+          <header className="ProjectForm__header">
+            <h2 className="page-header"></h2>
+          </header>
+          <MultiImageUpload files={files} setFiles={setFiles} />
+          {isEditing && (
+            <div>
+              <div className="thumbnail-container ">
+                {projectPhotos.map((photo, index) => (
+                  <div key={index}>
+                    <img
+                      alt=""
+                      src={remove}
+                      className="remove"
+                      onClick={(e) => {}}
+                    />
 
-                  <div className="thumb" key={index}>
-                    <div style={thumbInner}>
-                      <img
-                        alt="project thumbnail"
-                        src={photo.url}
-                        className="thumbnail"
-                      />
+                    <div className="thumb" key={index}>
+                      <div style={thumbInner}>
+                        <img
+                          alt="project thumbnail"
+                          src={photo.url}
+                          className="thumbnail"
+                        />
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
-          </div>
-        )}
-      </div>
-      <div className="right-container">
-        <form encType="multipart/form-data" className="project-form-container">
-          <div className="required">
-            <label htmlFor="name" className="label project-label">
-              Project title *
+          )}
+        </div>
+        <div className="right-container">
+          <form
+            encType="multipart/form-data"
+            className="project-form-container"
+          >
+            <div className="required">
+              <label htmlFor="name" className="label project-label">
+                Project title *
+              </label>
+              <input
+                required
+                autoFocus={true}
+                type="text"
+                value={name}
+                name="name"
+                id="name"
+                placeholder="Enter project title here"
+                onChange={handleChanges}
+                ref={setTitleRef}
+              />
+            </div>
+            <label htmlFor="description" className="label">
+              Project description
             </label>
-            <input
-              required
-              autoFocus={true}
+            <textarea
+              id="description"
+              name="description"
+              value={description}
               type="text"
-              value={name}
-              name="name"
-              id="name"
-              placeholder="Enter project title here"
+              placeholder="Enter description here"
               onChange={handleChanges}
-              ref={setTitleRef}
+              className="description"
+              maxLength="240"
             />
-          </div>
-          <label htmlFor="description" className="label">
-            Project description
-          </label>
-          <textarea
-            id="description"
-            name="description"
-            value={description}
-            type="text"
-            placeholder="Enter description here"
-            onChange={handleChanges}
-            className="description"
-            maxLength="240"
-          />
-          <CharacterCount string={description} limit={240} />
+            <CharacterCount string={description} limit={240} />
 
             {/*PROJECT CATEGORIES */}
             <label htmlFor="privacyLink" className="label">
@@ -243,69 +246,70 @@ const ProjectFromBody = ({
             >
               {/*if editing a project and a category was previously selected for the project
                    display that category as the default selection. if not, dispay the defaut option */}
-{/*              {isEditing && projectCategories[0] ? (
+              {/*              {isEditing && projectCategories[0] ? (
                 <option value="" disabled selected hidden>
                   {projectCategories[0].category}
                 </option>
               ) : (
+              */}
                 <option value="" disabled selected hidden>
                   Please Select a Category
                 </option>
               )}
-*/}
-                <option value="" disabled selected hidden>
-                  Please Select a Category
-                </option>
+
+              <option value="" disabled selected hidden>
+                Please Select a Category
+              </option>
               {categoryNames.map((category, index) => {
                 return (
                   <option key={category.id} value={category.id}>
-                    {category.category}
+                    {category}
                   </option>
                 );
               })}
             </select>
 
-          <label htmlFor="figmaLink" className="label">
-            Figma
-          </label>
-          <input
-            type="text"
-            name="figma"
-            value={figma}
-            placeholder="Enter link here (optional)"
-            id="figmaLink"
-            onChange={handleChanges}
-          />
-          <label htmlFor="invisionLink" className="label">
-            Prototype
-          </label>
-          <input
-            type="text"
-            name="invision"
-            value={invision}
-            placeholder="Enter link here (optional)"
-            id="invisionLink"
-            onChange={handleChanges}
-          />
+            <label htmlFor="figmaLink" className="label">
+              Figma
+            </label>
+            <input
+              type="text"
+              name="figma"
+              value={figma}
+              placeholder="Enter link here (optional)"
+              id="figmaLink"
+              onChange={handleChanges}
+            />
+            <label htmlFor="invisionLink" className="label">
+              Prototype
+            </label>
+            <input
+              type="text"
+              name="invision"
+              value={invision}
+              placeholder="Enter link here (optional)"
+              id="invisionLink"
+              onChange={handleChanges}
+            />
 
-          <CaseStudy />
-          <Privacy />
-          <Editing />
-          <div className="submit-cancel-container">
-            <button type="button" className="cancel-btn">
-              Cancel
-            </button>
-            <button className="submit-button" type="submit"></button>
-          </div>
-          <div className="error">error</div>
+            <CaseStudy />
+            <Privacy />
+            <Editing />
+            <div className="submit-cancel-container">
+              <button type="button" className="cancel-btn">
+                Cancel
+              </button>
+              <button className="submit-button" type="submit"></button>
+            </div>
+            <div className="error">error</div>
 
-          <div className="delete-project-button">
-            <DeleteIcon />
-            <p>Delete project</p>
-          </div>
-        </form>
-      </div>
-    </section>
+            <div className="delete-project-button">
+              <DeleteIcon />
+              <p>Delete project</p>
+            </div>
+          </form>
+        </div>
+      </section>
     </>
   );
 };
