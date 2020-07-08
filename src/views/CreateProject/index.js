9a92eda80114690c07825a5c8adcf185d0641c19
andrewbastian <andrew.bastian@gmail.com>
2020-07-08 11:29:44 -0700

    Getting and sending current user data to ProjectFormBody

--- src/views/CreateProject/index.js
+++ src/views/CreateProject/index.js
@@ -3,9 +3,20 @@ import Layout from '../../common/Layout';
 import DeleteProjectModal from './DeleteProjectModal';
 import InviteModal from './InviteModal';
 import ProjectFormBody from './ProjectFormBody';
+
+import { useAuth0 } from '../../utilities/auth-spa.js';
+import { useQuery } from '@apollo/react-hooks';
+import { GET_USER_BY_ID_QUERY } from '../../graphql/index';
+
 import './styles.scss';
 
 export default function CreateProject() {
+
+    const { logout, user } = useAuth0();
+
+  const { data: userData } = useQuery(GET_USER_BY_ID_QUERY, {
+    variables: { id: user?.sub },
+  });
   return (
     <Layout>
       <div className="project-form-wrapper">
@@ -13,7 +24,7 @@ export default function CreateProject() {
         <div className="project-form-wrapper">
           <InviteModal />
         </div>
-        <ProjectFormBody />
+        <ProjectFormBody userData={userData} user={user}/>
       </div>
     </Layout>
   );
