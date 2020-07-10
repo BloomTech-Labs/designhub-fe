5b9ade993e7d1b47ba5a43b8b4cc97aa0f63095c
AislynnEdmiston <davidandaislynn@gmail.com>
2020-07-09 23:18:20 -0400

    worked on notifications until renderbased on type

--- src/views/Notifications/index.js
+++ src/views/Notifications/index.js
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '../../common/Layout';
+import { axiosWithAuth } from '../../utilities/axiosWithAuth';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import empty from '../../ASSETS/Icons/empty_project.svg';
 import './styles.scss';
 
 export default function Notifications() {
+  const [state, setState] = useState([]);
+  // const { id } = props.activeUser;
+  // const { invite, getInvitesByUser } = props;
+
+  // useEffect(() => {
+  //   try {
+  //     const turnToRead = async data => {
+  //       if (data.unReadNotifications) {
+  //         return await Promise.all(
+  //           data.unReadNotifications.map(async i => {
+  //             await axiosWithAuth().put(`api/v1/invite/${i.id}`, {
+  //               unread: false
+  //             });
+  //           })
+  //         );
+  //       }
+  //     };
+  //     const getNotifications = async id => {
+  //       const { data } = await axiosWithAuth().get(`api/v1/invite/${id}`);
+  //       setState(data);
+  //       turnToRead(data);
+  //     };
+  //     getNotifications(id);
+  //     getInvitesByUser();
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // }, [id, invite, getInvitesByUser]);
+
+  // const handleInvites = (accept, invite) => {
+  //   if (accept) {
+  //     props.acceptInvite(invite.id);
+  //   } else {
+  //     props.deleteInvite(invite);
+  //   }
+  // };
+
   return (
     <Layout>
       <h1>Notifications!</h1>
