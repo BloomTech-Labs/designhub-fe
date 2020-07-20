d43624c737cf2ac577097fa57797bb4ae1e7b6dc
andrewbastian <andrew.bastian@gmail.com>
2020-07-20 13:50:43 -0700

    Working on Explore Category display

--- src/views/Explore/Illustration.js
+++ src/views/Explore/Illustration.js
@@ -3,7 +3,14 @@ import React from 'react';
 import empty from '../../ASSETS/Icons/empty_project.svg';
 import ProjectThumbnail from '../Project/ProjectThumbnail';
 
-export default function Illustration() {
+export default function Illustration({ allProjects, project, user, users, illustrations }) {
+  // let getIllustrations = allProjects?.projects.filter((project) => {
+  //   if (project?.category == 'Illustration') {
+  //     return project;
+  //   }
+  // });
+  // console.log(getIllustrations);
+
   return (
     <>
       <div className="empty-state">
@@ -12,8 +19,28 @@ export default function Illustration() {
       </div>
 
       <div className="explore-projects-array">
-        <ProjectThumbnail />;
+        {illustrations.map((project) => {
+          const user = users.find((user) => user.id === project.userId);
+          return (
+            <ProjectThumbnail projectData={project} user={user} key={project.id} />
+          );
+        })}{' '}
       </div>
     </>
   );
 }
+
+// export default function Illustration() {
+//   return (
+//     <>
+//       <div className="empty-state">
+//         <img src={empty} alt="empty" className="empty-icon" />
+//         <h1 className="no-projects">There are no projects in this category.</h1>
+//       </div>
+
+//       <div className="explore-projects-array">
+//         <ProjectThumbnail />;
+//       </div>
+//     </>
+//   );
+// }
